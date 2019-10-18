e0efb2136e75f6b35ef83b0905f6b34b72e6ebcb
Arjun Baghela <abaghela@dhcp-128-189-251-92.ubcsecure.wireless.ubc.ca>
2019-10-18 15:53:51 -0700

    fix mutation_pop to accept population class objects

--- R/mutation_pop.R
+++ R/mutation_pop.R
@@ -18,12 +18,14 @@
 #' @export
 
 
-mutation_pop <- function(list_of_org, md = c("constant", "guassian", "poisson"), mut_rate_avg, mut_rate_sd= NULL) {
+mutation_pop <- function(org_population, md = c("constant", "guassian", "poisson"), mut_rate_avg, mut_rate_sd= NULL) {
+  list_of_org <- org_population@organisms
+
 
   purrr::map(list_of_org, ~mutation(.x, md, mut_rate_avg, mut_rate_sd))
 
 }
 
-list_of_org <- list(glider, glider)
+list_of_org <- population(organisms= list(glider, glider))
 
 mutation_pop(list_of_org, md = c("constant"), 0.3)
