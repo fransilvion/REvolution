f790849d3cfe0669ba4ca3174385f9317f6a9792
Arjun Baghela <abaghela@dhcp-128-189-249-134.ubcsecure.wireless.ubc.ca>
2019-10-19 13:30:03 -0700

    change plotfitness

--- R/plotFitness.R
+++ R/plotFitness.R
@@ -19,29 +19,6 @@
 #'
 #'
 
-
-# # Example of a glider encoded as logical matrix
- glider_logical <- matrix( data = c(F,T,F,
-                                    F,F,T,
-                                    T,T,T), nrow = 3, byrow = T)
-
- # Example of a glider encoded as an organism
- glider <- organism(cells = glider_logical)
-
- # Inverse of the glider above
- glider_inv <-  organism(cells = !attr(glider, "cells"))
-
- gliders_G0 <- population( organisms = list( glider, glider_inv), fitness = c(NA,NA) )
-
- gliders_G1 <-  popMutation(gliders_G0, "constant", 0.3)
- gliders_G1@fitness <- c(2,2)
-
- gliders_G2 <- popMutation(gliders_G1, "constant", 0.3)
- gliders_G2@fitness <- c(3,5)
-
- si_life <- lineage( populations = list(gliders_G0, gliders_G1, gliders_G2), G = c(0,1,2) )
-
-
 plotFitness <- function(si_life, no_organisms) {
   df <- data.frame(fitness = si_life@populations %>% purrr::map(~.x@fitness) %>% unlist(),
                    org_number = rep(1:no_organisms, length(si_life@populations)),
