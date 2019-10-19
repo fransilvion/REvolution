fb60120ed1a9513896ffc7bc7a30114614ecd6df
Arjun Baghela <abaghela@dhcp-128-189-249-134.ubcsecure.wireless.ubc.ca>
2019-10-19 10:15:37 -0700

    convert list to population class in popMutation

--- R/popMutation.R
+++ R/popMutation.R
@@ -21,8 +21,10 @@ popMutation <- function(org_population, md = c("constant", "guassian", "poisson"
   list_of_org <- org_population@organisms
 
   purrr::map(list_of_org, ~Mutation(.x, md, mut_rate_avg, mut_rate_sd))
+  list_of_org <- population(organisms = list_of_org)
+  return(list_of_org)
 
 }
 
 #list_of_org <- population(organisms= list(glider, glider))
-#mutation_pop(list_of_org, md = c("constant"), 0.3)
+#hi <- popMutation(list_of_org, md = c("constant"), 0.3)
