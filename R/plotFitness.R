c96fd43f095fb5250376e8fe7d378aad82ca0712
Arjun Baghela <abaghela@dhcp-128-189-249-134.ubcsecure.wireless.ubc.ca>
2019-10-19 13:51:38 -0700

    fix plot function

--- R/plotFitness.R
+++ R/plotFitness.R
@@ -19,31 +19,13 @@
 #'
 #'
 
-# glider_logical <- matrix( data = c(F,T,F,
-#                                    F,F,T,
-#                                    T,T,T), nrow = 3, byrow = T)
-
- # Example of a glider encoded as an organism
- #glider <- organism(cells = glider_logical)
-
- # Inverse of the glider above
- #glider_inv <- organism(cells = !attr(glider, "cells"))
-
- #gliders_G0 <- population( organisms = list( glider, glider_inv), fitness = c(NA,NA) )
-
- #gliders_G1 <- popMutation(gliders_G0, "constant", 0.4)
-
- #si_life <- lineage( populations = list(gliders_G0, gliders_G1), G = c(0,1) )
-
-
-
 plotFitness <- function(si_life, no_organisms) {
-  df <- data.frame(fitness = si_life@populations %>% purrr::map(~.x@fitness) %>% unlist(),
-                   org_number = rep(1:no_organisms, length(si_life@populations)),
-                   gen = rep(si_life@G, no_organisms) %>% sort())
+  df <- data.frame(fitness = si_life@populations %>% purrr::map(~mean(.x@fitness)) %>% unlist(),
+                   #org_number = rep(1:no_organisms, length(si_life@populations)),
+                   gen = si_life@G)
   df[is.na(df)] <- 0
 
-  ggplot(df, aes(x = gen, y = fitness, group = org_number)) +
+  ggplot(df, aes(x = gen, y = fitness)) +
     geom_line() +
     scale_x_continuous(limits = c(0,max(df$gen)), breaks = seq(0,max(df$gen),1), labels= paste0("G", unique(c(df$gen))))
 
