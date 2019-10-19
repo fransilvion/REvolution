7eeb766df980a74996ad69f2609d67032ba4fc72
rachel-kt <rachel.kt1208@gmail.com>
2019-10-19 13:16:45 +0530

    Update rle2mat.R

--- R/rle2mat.R
+++ R/rle2mat.R
@@ -28,30 +28,39 @@ rle2mat <- function(file_path) {
   len_x = as.numeric(strsplit(pattern[5],",")[[1]][1])
   len_y = as.numeric(strsplit(pattern[8],",")[[1]][1])
   # Get coordinates of live cells
-  cells = strsplit(pattern[12:length(pattern)], split = "\\$", perl = T)
-
+  #cells = strsplit(pattern[12:length(pattern)], split = "\\$", perl = T)
+  cells = paste(pattern[12:length(pattern)], collapse = "")
+  cell_lines = unlist(strsplit(cells, "\\$"))
   # Construct matrix for organism
-  organism_l = Matrix(F, nrow = len_x, ncol = len_y, sparse = T)
-  blanks=0
-  for (char in cells) {
-    #print(char)
-    char_elem = strsplit(char, "o")[[1]]
-
-    for (steps in char_elem){
-      if(unlist(gregexpr("b", steps)) > 0) {
-        blanks = strsplit(steps, "b")[[1]]
-      } else {
-        blanks = 0
-      }
-      #print(blanks)
-      }
-    }
-
-  # Convert the logical matrix into an organism object
-  org <- organism( cells = organism_l )
-  return(org)
+  organism_l = Matrix(F, nrow = len_y, ncol = len_x, sparse = T)
+  i=1
+  for (cells in cell_lines)
+    {
+        if(substr(cells, 1,1) == "o")
+        {
+          tmp_cell = paste("1",cells, sep = "")
+        }else{tmp_cell = cells}
+        if(substr(tmp_cell, 2,2) == "o")
+        {
+          cell_start = T
+        } else {cell_start = F}
+        tmp_cell = gsub("bo", replacement = "b1o", tmp_cell)
+        tmp_cell = gsub("ob", replacement = "o1b", tmp_cell)
+        tmp_cell = gsub("!", replacement = "", tmp_cell)
+        z$lengths <- as.numeric(unlist(strsplit(gsub("[o/b/!]", " ", tmp_cell[[1]][1]), " ")))
+        z$lengths <- z$lengths[!is.na(z$lengths)]
+        z$values <- rep(c(cell_start,!cell_start), times = ceiling(length(z$lengths)/2))[1:length(z$lengths)]
+        z
+        cells_occ = inverse.rle(z)
+        organism_l[i,1:length(cells_occ)] = cells_occ
+        if(substr(tmp_cell,nchar(tmp_cell), nchar(tmp_cell))!="o")
+          {
+            empty_row = as.numeric(substr(tmp_cell,nchar(tmp_cell), nchar(tmp_cell)))
+            i = i+empty_row
+          } else {empty_row = 0;i=i+1 }
+  }
+  return(organism_l)
 }
-
 file_path  = "./pattern.rle"
-
-
+file_path = "./origin.rle"
+rle2mat(file_path = file_path)
