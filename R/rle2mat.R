9f65a8713bd812c7225db76192c54d57405df2b4
rachel-kt <rachel.kt1208@gmail.com>
2019-10-19 14:28:42 +0530

    Update rle2mat.R

--- R/rle2mat.R
+++ R/rle2mat.R
@@ -23,13 +23,22 @@ rle2mat <- function(file_path) {
   # Read each line of .rle file
   pattern = scan(file = file_path, what = character())
   # Get start position of organism
-  pos = strsplit(pattern[2],"=")[[1]][2]
+  pos_loc = 2
+  len_x_loc = 5
+  len_y_loc = 8
+  cell_loc = 12
+  loc = c(len_x_loc, len_y_loc, cell_loc)
+  if(unlist(gregexpr("Gen",pattern[3]))>0)
+  {
+    loc = loc+1
+  }
+  pos = strsplit(pattern[pos_loc],"=")[[1]][2]
   # Get dimensions of the organism
-  len_x = as.numeric(strsplit(pattern[5],",")[[1]][1])
-  len_y = as.numeric(strsplit(pattern[8],",")[[1]][1])
+  len_x = as.numeric(strsplit(pattern[loc[1]],",")[[1]][1])
+  len_y = as.numeric(strsplit(pattern[loc[2]],",")[[1]][1])
   # Get coordinates of live cells
   #cells = strsplit(pattern[12:length(pattern)], split = "\\$", perl = T)
-  cells = paste(pattern[12:length(pattern)], collapse = "")
+  cells = paste(pattern[loc[3]:length(pattern)], collapse = "")
   cell_lines = unlist(strsplit(cells, "\\$"))
   # Construct matrix for organism
   organism_l = Matrix(F, nrow = len_y, ncol = len_x, sparse = T)
@@ -58,6 +67,9 @@ rle2mat <- function(file_path) {
             i = i+empty_row
           } else {empty_row = 0;i=i+1 }
   }
+  # Convert the logical matrix into an organism object
+  #org <- organism( cells = organism_l )
+  #return(org)
   return(organism_l)
 }
 file_path  = "./pattern.rle"
