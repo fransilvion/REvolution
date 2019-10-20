28575299c47fe42a5f34560d6e9e8ff4dbac94f9
ababaian <ababaian@bccrc.ca>
2019-10-20 12:52:02 -0700

    updated rGolly

--- R/rGolly.R
+++ R/rGolly.R
@@ -14,37 +14,49 @@
 #'
 #'
 #' @export
-
-rGolly <- function(org_bin_matr= NULL, generation = NULL, stepsize=NULL, maxmemory= NULL,
+require(purrr)
+require(utils)
+require(dplyr)
+rGolly <- function(bgolly_path = "bgolly/bgolly",
+                   input_rle = NULL, input_organism = NULL,
+                   output = "stdout",
+                   generation = NULL, stepsize=NULL, maxmemory= NULL,
                    maxtime = NULL, benchmark = NULL, exponential = NULL, quiet = NULL,
                    rule = NULL, search = NULL, hashlife = NULL, algorithm = NULL,
-                   output = NULL, verbose = NULL, timeline = NULL, render = NULL,
+                   verbose = NULL, timeline = NULL, render = NULL,
                    progress = NULL, popcount = NULL, scale= NULL, autofit = NULL,
                    exec = NULL) {
 
-  # Is a the appropriate object/matrix provided?
-  #if(is.null(org_bin_matr)) {
-  #  return("A organism or object is required")
-  #}
+  # Check that an input organism or RLE input file is provided
+  if ( is.null(input_rle) | is.null(input_organism) ){
+    stop("No input pattern provided. Use `organism = <glider>` or `input_rle = PATH_TO_RLE`")
+  } else if ( !is.null(input_rle) ){
+    # input RLE object is provided, read RLE into bgolly
+    INPUT <- input_rle
+  } else if ( !is.null(input_organism) ){
+    # input organism object is provided, stream into bgolly directly
+    INPUT <- "stdin"
+  }
 
-  # Get provided parameters
+  # Read function parameters
   options(warn=-1)
   params <- as.list(match.call()) %>%
     purrr::list_modify("org_bin_matr" = NULL) %>%
     utils::stack() %>%
     dplyr::mutate(cmd = paste0(" --", ind, " ", values))
   options(warn=0)
-
   # Get command as a string
-  params <- paste(params$cmd, collapse = "")
+  params <- paste(params$cmd, "-q -q", collapse = "")
 
-  if(is.null(output)) {
-    sys_comm <- paste0("bgolly", params, " --output stdout "," ", org_bin_matr)
+
+  # If no output, print to standard output
+  if (output == "stdout"){
+    sys_comm <- paste0("bgolly", params, " --output stdout "," ", INPUT)
+    testing <- paste0("system(", sys_coom, "intern = T, ignore.stderr =T )")
   } else {
-    sys_comm <-  paste0("bgolly", params, " --output ", output," ", org_bin_matr)
+    sys_comm <-  paste0("bgolly", params, " --output ", output," ", INPUT)
+    testing <- paste0("system(", sys_coom, ")")
   }
-
-
 
   print(sys_comm)
   #system(sys_comm)
