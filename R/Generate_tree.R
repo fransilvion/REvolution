06d30eb9de6c63c0ca76b6c9d68f5990e31934d8
ababaian <ababaian@bccrc.ca>
2019-10-20 14:39:42 -0700

    tmp namespace

--- R/Generate_tree.R
+++ R/Generate_tree.R
@@ -19,14 +19,14 @@
 Generate_tree<- function(inANS, warnings=TRUE){
   #Requires ape library
   require('ape')
-  
+
   #Initalise history from final generation selection
   ancestey <- inANS@map
   hist <- as.data.frame(cbind(unique(ancestey[nrow(ancestey),])))
   colnames(hist) <- c("last")
   idx <- cbind(seq(1,length(unique(ancestey[nrow(ancestey),]))))
   ancestey <- ancestey[-c(nrow(ancestey)),]
-  
+
   #Populate history
   for(g in seq(nrow(ancestey),1)){
     g_idx <- c()
@@ -34,21 +34,21 @@ Generate_tree<- function(inANS, warnings=TRUE){
       g_idx <- append(g_idx, ancestey[g,hist[o,1]])
     }
     hist$gidx <- g_idx
-    
+
     #Every generatrion merge branches
     if(length(hist$gidx) > length(unique(hist$gidx))){
-      
+
       #Only activate for values where
       #there are items to merge
       for(val in unique(hist$gidx)){
-        
+
         #Identify items to merge
         to_merge <- which(hist$gidx == val)
         if(length(to_merge) > 1){
-          
+
           #Merge index values
           idx[to_merge][1] <- paste(idx[to_merge], collapse=',')
-          
+
           #Remove merged values
           to_merge <- to_merge[-1]
           idx <- idx[-to_merge]
@@ -56,12 +56,12 @@ Generate_tree<- function(inANS, warnings=TRUE){
         }
       }
     }
-    
+
     #Remove old genetation and
     #prepare for next generation
     hist$last <- hist$gidx
     idx <- sprintf('(%s)', idx)
-    
+
     #Break and warn if 1 branch is left
     if(length(idx) == 1){
       if(warnings == TRUE){
@@ -70,15 +70,15 @@ Generate_tree<- function(inANS, warnings=TRUE){
       break
     }
   }
-  
+
   #Convert index to string
   str <- paste( c('(',paste( unlist(idx), collapse=','),');'), collapse='')
-  
+
   #Generate tree from string and return
   return(read.tree(text = str))
 }
-tree2 <- Generate_tree(df)
-plot(tree2)
+# tree2 <- Generate_tree(df)
+# plot(tree2)
 #Example plots for ape
 #Phylogram
 #plot(tree)
