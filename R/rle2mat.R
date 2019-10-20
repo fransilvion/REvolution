3b3daaa1a5f21cfcfd8ea0110defc37e074c0aab
rachel-kt <rachel.kt1208@gmail.com>
2019-10-20 22:02:17 +0530

    added suicide fix

--- R/rle2mat.R
+++ R/rle2mat.R
@@ -40,34 +40,39 @@ rle2mat <- function(file_path) {
   cell_lines = unlist(strsplit(cells, "\\$"))
   # Construct matrix for organism
   #organism_l = Matrix(F, nrow = len_y, ncol = len_x, sparse = T)
-  organism_l = matrix(F, nrow = len_y, ncol = len_x)
-  i=1
-  for (cells in cell_lines)
+  if(cell_lines == "!")
   {
-    if((substr(cells, 1,1) == "o")||(substr(cells, 1,1) == "b"))
-    {
-      tmp_cell = paste("1",cells, sep = "")
-    }else{tmp_cell = cells}
-    if(substr(tmp_cell, 2,2) == "o")
-    {
-      cell_start = T
-    } else {cell_start = F}
-    tmp_cell = gsub("bo", replacement = "b1o", tmp_cell)
-    tmp_cell = gsub("ob", replacement = "o1b", tmp_cell)
-    tmp_cell = gsub("!", replacement = "", tmp_cell)
-    if(substr(tmp_cell,nchar(tmp_cell), nchar(tmp_cell))!="o")
+    organism_l = matrix(F, nrow = len_y+1, ncol = len_x+1)
+  }else{
+      organism_l = matrix(F, nrow = len_y, ncol = len_x)
+      i=1
+      for (cells in cell_lines)
+      {
+        if((substr(cells, 1,1) == "o")||(substr(cells, 1,1) == "b"))
         {
-          tmp_l = strsplit(gsub("[o/b]", " ", tmp_cell)," ")[[1]]
-          empty_row = as.numeric(tmp_l[length(tmp_l)])
-          tmp_cell = substr(tmp_cell,1,nchar(tmp_cell)-nchar(tmp_l[length(tmp_l)]))
-        } else {empty_row = 1}
-    z <- list()
-    z$lengths <- as.numeric(unlist(strsplit(gsub("[o/b/!]", " ", tmp_cell[[1]][1]), " ")))
-    z$lengths <- z$lengths[!is.na(z$lengths)]
-    z$values <- rep(c(cell_start,!cell_start), times = ceiling(length(z$lengths)/2))[1:length(z$lengths)]
-    cells_occ = inverse.rle(z)
-    organism_l[i,1:length(cells_occ)] = cells_occ
-    i = i+empty_row
+          tmp_cell = paste("1",cells, sep = "")
+        }else{tmp_cell = cells}
+        if(substr(tmp_cell, 2,2) == "o")
+        {
+          cell_start = T
+        } else {cell_start = F}
+        tmp_cell = gsub("bo", replacement = "b1o", tmp_cell)
+        tmp_cell = gsub("ob", replacement = "o1b", tmp_cell)
+        tmp_cell = gsub("!", replacement = "", tmp_cell)
+        if(substr(tmp_cell,nchar(tmp_cell), nchar(tmp_cell))!="o")
+            {
+              tmp_l = strsplit(gsub("[o/b]", " ", tmp_cell)," ")[[1]]
+              empty_row = as.numeric(tmp_l[length(tmp_l)])
+              tmp_cell = substr(tmp_cell,1,nchar(tmp_cell)-nchar(tmp_l[length(tmp_l)]))
+            } else {empty_row = 1}
+        z <- list()
+        z$lengths <- as.numeric(unlist(strsplit(gsub("[o/b/!]", " ", tmp_cell[[1]][1]), " ")))
+        z$lengths <- z$lengths[!is.na(z$lengths)]
+        z$values <- rep(c(cell_start,!cell_start), times = ceiling(length(z$lengths)/2))[1:length(z$lengths)]
+        cells_occ = inverse.rle(z)
+        organism_l[i,1:length(cells_occ)] = cells_occ
+        i = i+empty_row
+       }
   }
 
   shift_l <- c( -nrow(organism_l)%/%2, -ncol(organism_l)%/%2)
